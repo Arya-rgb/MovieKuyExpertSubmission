21a951493bb5e2b4ba35d845d4473986045e14cb
arya-rgb <dickyarya.personal@gmail.com>
2021-05-28 04:49:24 +0700

    menambah dummy unit test dan persiapan circle ci

---
+++ favorite/src/test/java/com/thorin/favorite/UnitTest.kt
@@ -0,0 +1,13 @@
+package com.thorin.favorite
+
+import org.junit.Assert
+import org.junit.Test
+
+//untuk cek apakah unit test berjalan di circle ci
+
+class UnitTest {
+    @Test
+    fun addition_isCorrect() {
+        Assert.assertEquals(4, 2 + 2)
+    }
+}
