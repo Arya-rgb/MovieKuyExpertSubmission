1bd319a9c2438d46df87f189bf2584c3e8f8bea1
arya-rgb <dickyarya.personal@gmail.com>
2021-05-28 02:04:48 +0700

    Menerapkan encryption pada database

--- core/src/main/java/com/thorin/core/di/CoreModule.kt
+++ core/src/main/java/com/thorin/core/di/CoreModule.kt
@@ -8,6 +8,8 @@ import com.thorin.core.data.source.remote.RemoteDataSource
 import com.thorin.core.data.source.remote.network.ApiService
 import com.thorin.core.domain.repository.IFilmRepository
 import com.thorin.core.utils.AppExecutors
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -20,10 +22,14 @@ import java.util.concurrent.TimeUnit
 val databaseModule = module {
     factory { get<FilmDatabase>().filmDao() }
     single {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("dickyganteng".toCharArray())
+        val factory = SupportFactory(passphrase)
         Room.databaseBuilder(
             androidContext(),
             FilmDatabase::class.java, "Film.db"
-        ).fallbackToDestructiveMigration().build()
+        ).fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
     }
 }
 
